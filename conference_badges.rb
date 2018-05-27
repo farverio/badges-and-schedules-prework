9f99bc79e6b4bb36aaaf436845114548b625469e
Stephen F <stephen@farver.io>
2018-05-27 21:20:06 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,12 @@
-# Write your code here.
+speakers = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+
+def batch_badge_creator(array)
+  output = []
+  
+  array.each_with_index do |el, idx|
+    output.push("Hello, #{el}! You'll be assigned to room #{idx+1}!")
+  end 
+  
+  output
+end
+    
