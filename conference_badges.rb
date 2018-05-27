929fa8957a915cdc386ea7221ecedcbdebc043f0
Stephen F <stephen@farver.io>
2018-05-27 21:34:07 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -21,6 +21,6 @@ def assign_rooms(array)
 end
 
 def printer(array)
-  puts batch_badge_creator(array)
-  puts assign_rooms(array)
+  print batch_badge_creator(array)
+  print assign_rooms(array)
 end
